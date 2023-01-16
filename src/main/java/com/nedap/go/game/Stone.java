ea77454779292a04ef8b62a61ba37b98a8defbb5
arjonne.laar <a.laar@student.utwente.nl>
2023-01-16 17:10:28 +0100

    Create Stone ENUM class

---
+++ src/main/java/com/nedap/go/game/Stone.java
@@ -0,0 +1,10 @@
+package com.nedap.go.game;
+
+/**
+ * Represents the stones in the GO game. There are three possibilities:
+ * Stone.BLACK, Stone.WHITE and Stone.EMPTY.
+ */
+
+public enum Stone {
+    BLACK, WHITE, EMPTY;
+}
