b6c1f038694d678974e059d71a8fe2553f181bc8
arjonne.laar <a.laar@student.utwente.nl>
2023-01-17 13:24:59 +0100

    Update Board and BoardTest on checks and function names

--- src/main/java/com/nedap/go/game/Board.java
+++ src/main/java/com/nedap/go/game/Board.java
@@ -4,154 +4,135 @@ package com.nedap.go.game;
  * Represents the board of the GO game.
  */
 public class Board {
-    public static final int DIMENSION = 9; // represents the number of rows and columns of the board.
-
-    // the board is represented as a 2D array:
-    private Stone[][] board;
-    private Board copyBoard; // can be removed in the end! only used for checking if copy goes well by printing this.
+    public static final int SIZE = 9; // represents the number of rows and columns of the board.
+    private final Stone[][] board; // the board is represented as a 2D array.
 
     /**
      * Creates an empty board.
      */
     public Board() {
-        board = new Stone[DIMENSION][DIMENSION];
-        for (int row = 0; row < DIMENSION; row++) {
-            for (int column = 0; column < DIMENSION; column++) {
+        board = new Stone[SIZE][SIZE];
+        for (int row = 0; row < SIZE; row++) {
+            for (int column = 0; column < SIZE; column++) {
                 board[row][column] = Stone.EMPTY;
             }
         }
     }
 
     /**
-     * Creates a deep copy of this field.
+     * Creates a copy of this field.
      */
-    public Board deepCopy() {
-        copyBoard = new Board();
-        for (int row = 0; row < DIMENSION; row++) {
-            for (int column = 0; column < DIMENSION; column++) {
+    public Board copyBoard() {
+        Board copyBoard = new Board();
+        for (int row = 0; row < SIZE; row++) {
+            for (int column = 0; column < SIZE; column++) {
                 copyBoard.board[row][column] = board[row][column];
             }
         }
         return copyBoard;
     }
 
-    public void printBoard() { // can be deleted in the end; only check to see how/if it works as expected.
-        // print the column numbers on top
-        System.out.print("   ");
-        for (int column = 0; column < DIMENSION; column++) {
-            System.out.printf("%-3s", column);
-        }
-        System.out.println();
-
-        // print the row numbers on the left side.
-        for (int row = 0; row < DIMENSION; row++) {
-            System.out.printf("%-3s", row);
-
-            // print the state of the field.
-            for (int column = 0; column < DIMENSION; column++) {
-                if ((board[row][column]) == Stone.EMPTY) {
-                    System.out.printf("%-2s", '.');
-                } else if ((board[row][column]) == Stone.BLACK) {
-                    System.out.printf("%-2s", 'B');
-                } else {
-                    System.out.printf("%-2s", 'W');
-                }
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
-    }
-
-    public void printCopyBoard() { // can be deleted in the end; only check to see how/if it works as expected.
-        // print the column numbers on top
-        System.out.print("   ");
-        for (int column = 0; column < DIMENSION; column++) {
-            System.out.printf("%-3s", column);
-        }
-        System.out.println();
-
-        // print the row numbers on the left side.
-        for (int row = 0; row < DIMENSION; row++) {
-            System.out.printf("%-3s", row);
-
-            // print the state of the field.
-            for (int column = 0; column < DIMENSION; column++) {
-                if ((copyBoard.board[row][column]) == Stone.EMPTY) {
-                    System.out.printf("%-2s", '.');
-                } else if ((copyBoard.board[row][column]) == Stone.BLACK) {
-                    System.out.printf("%-2s", 'B');
-                } else {
-                    System.out.printf("%-2s", 'W');
-                }
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
-    }
-
     /**
-     * Checks whether the field a player wants to set a stone does exist.
-     * @param row is the row of interest;
+     * Checks whether the position a player wants to place a stone is valid (= does exist).
+     *
+     * @param row    is the row of interest;
      * @param column is the column of interest;
-     * @return true if the combination of row and column does exist; otherwise, isField() returns false.
+     * @return true if the combination of row and column does exist; otherwise, isValidPosition() returns false.
      */
-    public boolean isPosition(int row, int column) {
-        if (row >= 0 && row < DIMENSION && column >= 0 && column < DIMENSION) {
+    public boolean isValidPosition(int row, int column) {
+        if (row >= 0 && row < SIZE && column >= 0 && column < SIZE) {
             return true;
         }
-        System.out.println("This is not a valid position. Both the row number and column number should be in the range 0 - " + (DIMENSION-1) + ".");
+        System.out.println("The combination of row " + row + " and column " + column + " does not represent a valid position. Both the row number and column number should be in the range 0 - " + (SIZE - 1) + ".");
         return false;
     }
 
     /**
-     * Gets the mark on a specific position
-     * @param row is the row of interest;
+     * Gets the stone on a specific position.
+     *
+     * @param row    is the row of interest;
      * @param column is the column of interest;
-     * @return the Stone that is placed on that position; can be Empty as well if no stone is placed.
+     * @return the Stone that is placed on that position; can be EMPTY as well if no stone is placed.
      */
-    public Stone getPosition(int row, int column) {
+    public Stone getStone(int row, int column) {
+        if (!isValidPosition(row, column)) {
+            //TODO what to return? Of hoort deze check hier sowieso niet, maar in Game class?
+        }
         return board[row][column];
     }
 
     /**
      * Checks whether the position is empty
-     * @param row is the row of interest;
+     *
+     * @param row    is the row of interest;
      * @param column is the column of interest;
-     * @return true if the position is empty; if result != Stone.EMPTY, return false.
+     * @return true if the position is empty; if position is not valid OR result != Stone.EMPTY, return false.
      */
     public boolean isEmptyPosition(int row, int column) {
-        if (getPosition(row, column) == Stone.EMPTY) {
-            return true;
+        if (!isValidPosition(row, column)) {
+            return false; //TODO check of dit inderdaad 'false' is? of hoort dit hier uberhaupt niet maar in Game class?
         }
-        System.out.println("This position is already in use: the stone on this position is " + getPosition(row, column));
-        return false;
+        if (getStone(row, column) != Stone.EMPTY) {
+            System.out.println("This position is already in use: the stone on this position is " + getStone(row, column) + ".");
+            return false;
+        }
+        return true;
     }
 
     /**
-     * Sets the stone of interest on a specific position (after checking if this position does exist and if this
-     * position is still empty).
-     * @param row is the row of interest;
+     * Places the stone of interest on a specific position (after checking if this position is valid and if this
+     * position is empty).
+     *
+     * @param row    is the row of interest;
      * @param column is the column of interest;
-     * @param stone is the stone of interest.
+     * @param stone  is the stone of interest.
      */
-    public void setPosition(int row, int column, Stone stone) {
-        if (isPosition(row, column) && isEmptyPosition(row, column)) {
+    public void placeStone(int row, int column, Stone stone) {
+        if (isValidPosition(row, column) && isEmptyPosition(row, column)) {
             board[row][column] = stone;
         }
     }
 
+    //
+//    public void printBoard() { // can be deleted in the end; only check to see how/if it works as expected.
+//        // print the column numbers on top
+//        System.out.print("   ");
+//        for (int column = 0; column < SIZE; column++) {
+//            System.out.printf("%-3s", column);
+//        }
+//        System.out.println();
+//
+//        // print the row numbers on the left side.
+//        for (int row = 0; row < SIZE; row++) {
+//            System.out.printf("%-3s", row);
+//
+//            // print the state of the field.
+//            for (int column = 0; column < SIZE; column++) {
+//                if ((board[row][column]) == Stone.EMPTY) {
+//                    System.out.printf("%-2s", '.');
+//                } else if ((board[row][column]) == Stone.BLACK) {
+//                    System.out.printf("%-2s", 'B');
+//                } else {
+//                    System.out.printf("%-2s", 'W');
+//                }
+//                System.out.print(" ");
+//            }
+//            System.out.println();
+//        }
+//    }
 //    public static void main(String[] args) {
 //        Board board = new Board();
 //        board.printBoard();
-//        board.deepCopy();
-//        board.printCopyBoard();
-//        board.setPosition(3,4,Stone.BLACK);
-//        board.setPosition(3,1,Stone.WHITE);
-//        board.getPosition(3,4);
-//        board.setPosition(11,3,Stone.WHITE);
-//        board.setPosition(3,4,Stone.WHITE);
+//        board.copyBoard();
+//        board.placeStone(3, 4, Stone.BLACK);
+//        board.placeStone(3, 1, Stone.WHITE);
+//        board.getStone(3, 4);
+//        board.placeStone(11, 3, Stone.WHITE);
+//        board.placeStone(3, 4, Stone.WHITE);
 //        board.printBoard();
-//        board.deepCopy();
-//        board.printCopyBoard();
+//        board.copyBoard();
+//        board.isEmptyPosition(11, 2);
+//        board.placeStone(14, 2, Stone.BLACK);
+//        board.getStone(11, 2);
 //    }
 }
--- src/test/java/com/nedap/go/game/BoardTest.java
+++ src/test/java/com/nedap/go/game/BoardTest.java
@@ -18,54 +18,56 @@ public class BoardTest {
     }
 
     /**
-     * Test to check if isPosition() really finds the actual positions on the board (rows and columns ranging between 0 and DIMENSION).
+     * Test to check if isValidPosition() really finds the actual positions on the board (rows and columns ranging between 0 and SIZE).
      */
     @Test
-    public void testIsPosition() {
-        assertFalse(board.isPosition(-1, 0));
-        assertTrue(board.isPosition(0, 0));
-        assertTrue(board.isPosition(3, Board.DIMENSION - 2));
-        assertFalse(board.isPosition(4, Board.DIMENSION));
+    public void testIsValidPosition() {
+        assertFalse(board.isValidPosition(-1, 0));
+        assertTrue(board.isValidPosition(0, 0));
+        assertTrue(board.isValidPosition(3, Board.SIZE - 2));
+        assertFalse(board.isValidPosition(4, Board.SIZE));
     }
 
     /**
-     * Test to check whether a stone can be placed on a position using setPosition() and to check if the correct stone is returned when using getPosition().
+     * Test to check whether a stone can be placed on a position using placeStone() and to check if the correct stone is returned when using getStone().
      */
     @Test
-    public void testSetAndGetPosition() {
-        board.setPosition(0, 0, Stone.BLACK);
-        assertEquals(Stone.BLACK, board.getPosition(0, 0));
-        assertEquals(Stone.EMPTY, board.getPosition(0, 1));
+    public void testPlaceAndGetStone() {
+        board.placeStone(0, 0, Stone.BLACK);
+        assertEquals(Stone.BLACK, board.getStone(0, 0));
+        assertEquals(Stone.EMPTY, board.getStone(0, 1));
+        // no checks for setting a stone on / getting a stone from a position out of boundaries of the board OR setting
+        // a stone on a non-empty position as this has been checked by other tests before.
     }
 
     /**
-     * Test to check whether deepCopy() does correctly make a copy of the current state of the board.
+     * Test to check whether copyBoard() does correctly make a copy of the current state of the board.
      */
     @Test
-    public void testDeepCopy() {
-        board.setPosition(1, 3, Stone.WHITE);
-        board.setPosition(1, 4, Stone.WHITE);
-        board.setPosition(1, 5, Stone.WHITE);
-        assertEquals(Stone.WHITE, board.getPosition(1, 3));
-        assertEquals(Stone.WHITE, board.getPosition(1, 4));
-        assertEquals(Stone.WHITE, board.getPosition(1, 5));
-        Board copyBoard = board.deepCopy();
-        assertEquals(Stone.WHITE, copyBoard.getPosition(1, 3));
-        assertEquals(Stone.WHITE, copyBoard.getPosition(1, 4));
-        assertEquals(Stone.WHITE, copyBoard.getPosition(1, 5));
-        board.setPosition(1, 6, Stone.WHITE);
-        assertNotEquals(Stone.WHITE, copyBoard.getPosition(1, 6));
-        copyBoard = board.deepCopy();
-        assertEquals(Stone.WHITE, copyBoard.getPosition(1, 6));
+    public void testCopyBoard() {
+        // First, place stones on the board. After copying the board, check whether these placed stones are on the copy
+        // of the board as well.
+        board.placeStone(1, 3, Stone.WHITE);
+        board.placeStone(1, 4, Stone.WHITE);
+        Board copyBoard = board.copyBoard();
+        assertEquals(Stone.WHITE, copyBoard.getStone(1, 3));
+        assertEquals(Stone.WHITE, copyBoard.getStone(1, 4));
+        // Another test, to confirm that after placing the stone on the board, it is not directly visible on the copy
+        // of the board too. It is, however, after copying the board.
+        board.placeStone(1, 5, Stone.WHITE);
+        assertNotEquals(Stone.WHITE, copyBoard.getStone(1, 5));
+        copyBoard = board.copyBoard();
+        assertEquals(Stone.WHITE, copyBoard.getStone(1, 5));
     }
 
     /**
-     * Test to check whether isEmptyPosition() does return true if no stone is placed on that position, and false if there is a stone on that position.
+     * Test to check whether isEmptyPosition() returns true if no stone is placed on that position, and false if there is a stone on that position.
      */
     @Test
     public void testIsEmptyPosition() {
         assertTrue(board.isEmptyPosition(2, 2));
-        board.setPosition(2, 2, Stone.BLACK);
+        board.placeStone(2, 2, Stone.BLACK);
         assertFalse(board.isEmptyPosition(2, 2));
+        assertFalse(board.isEmptyPosition(-1,2));
     }
 }
