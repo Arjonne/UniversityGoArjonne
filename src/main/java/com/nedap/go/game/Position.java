15c6b64c891cca770aacc66bd08f47ff9d5456b9
arjonne.laar <a.laar@student.utwente.nl>
2023-01-18 15:53:13 +0100

    Create class Position to be able to store positions with stones in a map

---
+++ src/main/java/com/nedap/go/game/Position.java
@@ -0,0 +1,23 @@
+package com.nedap.go.game;
+
+/**
+ * Represents the position on the board where a stone can be placed.
+ */
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+}
