3f00627d632a4c8078148d80a47a8602e9850844
arjonne.laar <a.laar@student.utwente.nl>
2023-01-27 12:49:44 +0100

    Add methods to determine the next move for both human player and computer player

---
+++ src/main/java/com/nedap/go/game/ComputerPlayer.java
@@ -0,0 +1,36 @@
+package com.nedap.go.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents the computer player of the GO game.
+ * This player has a name and has a stone (either black or white), and also a methods to determine a next move.
+ */
+
+public class ComputerPlayer extends Player {
+
+    /**
+     * Constructor to create a new computer player. Each player has a name and uses either a black or a white stone.
+     *
+     * @param username represents the name of the player
+     * @param stone    represents the stone this player uses.
+     */
+    public ComputerPlayer(String username, Stone stone) {
+        super(username, stone);
+    }
+
+    /**
+     * Determines the next move this player will make.
+     *
+     * @param game is the game this player is playing
+     * @return the move this player can make. If no valid move is available, return null and pass
+     */
+    @Override
+    public Position determineMove(Game game) {
+        if (game.findRandomValidPosition() == null) {
+            game.pass();
+        }
+            return game.findRandomValidPosition();
+    }
+}
--- src/main/java/com/nedap/go/game/Player.java
+++ src/main/java/com/nedap/go/game/Player.java
@@ -1,5 +1,10 @@
 package com.nedap.go.game;
 
+import javafx.geometry.Pos;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Represents the player of the GO game.
  * Each game has two players; either two human players, one human player and one computer player or two computer players.
@@ -37,5 +42,40 @@ public class Player {
      */
     public Stone getStone() {
         return stone;
+    }
+
+    /**
+     * Determines the next move this player will make.
+     *
+     * @param game is the game this player is playing
+     * @return the move this player can make. If no valid move is available, return null and pass
+     */
+    public Position determineMove(Game game) {
+        System.out.println("Do you want to make a move or pass? Type either MOVE or PASS:");
+        Scanner input = new Scanner(System.in);
+        int row = -1;
+        int column = -1;
+        if (input.nextLine().toUpperCase().equals("PASS")) {
+            game.pass();
+        } else if (input.nextLine().toUpperCase().equals("MOVE")) {
+            System.out.println("On what intersection do you want to place your stone?");
+            boolean correctInput = false;
+            while (!correctInput) {
+                System.out.print("First, enter the row number (ranging between 1 and " + Board.SIZE + ": ");
+                try {
+                    row = (input.nextInt() - 1);
+                } catch (InputMismatchException e) {
+                    System.out.println("Input must be a number between 1 and " + Board.SIZE + "!");
+                }
+                System.out.print("Now, enter the column number (ranging between 1 and " + (Board.SIZE) + ": ");
+                try {
+                    column = (input.nextInt() - 1);
+                    correctInput = true;
+                } catch (InputMismatchException e) {
+                    System.out.println("Input must be a number between 1 and " + Board.SIZE + "!");
+                }
+            }
+        }
+        return new Position(row, column);
     }
 }
