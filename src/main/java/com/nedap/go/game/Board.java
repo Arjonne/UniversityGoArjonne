3ea95e93cf26e46a39ed1618684d958a14bfc8b2
arjonne.laar <a.laar@student.utwente.nl>
2023-01-31 21:52:35 +0100

    Change in order of methods + improve documentation

--- src/main/java/com/nedap/go/game/Board.java
+++ src/main/java/com/nedap/go/game/Board.java
@@ -19,25 +19,38 @@ public class Board {
         }
     }
 
+// Getters:
+
     /**
-     * Creates a copy of the current state of the board.
+     * Gets the stone that is placed on a specific position.
+     *
+     * @param row    is the row of interest;
+     * @param column is the column of interest;
+     * @return the Stone that is placed on that position; can be EMPTY as well if no stone is placed.
      */
-    public Board copyBoard() {
-        Board copyBoard = new Board();
-        for (int row = 0; row < SIZE; row++) {
-            for (int column = 0; column < SIZE; column++) {
-                copyBoard.board[row][column] = board[row][column];
-            }
-        }
-        return copyBoard;
+    public Stone getStone(int row, int column) {
+        return board[row][column];
+    }
+
+// Methods needed to check whether a move is a valid move:
+
+    /**
+     * Checks whether the position is empty.
+     *
+     * @param row    is the row of interest;
+     * @param column is the column of interest;
+     * @return true if the position is empty; if not, return false.
+     */
+    public boolean isEmptyPosition(int row, int column) {
+        return getStone(row, column) == Stone.EMPTY;
     }
 
     /**
      * Checks whether the position a player wants to place a stone is valid (is within the boundaries of the board).
      *
-     * @param row    is the row of interest
-     * @param column is the column of interest
-     * @return true if the combination of row and column does exist on the board; if not, return false
+     * @param row    is the row of interest;
+     * @param column is the column of interest;
+     * @return true if the combination of row and column does exist on the board; if not, return false.
      */
     public boolean isValidPosition(int row, int column) {
         if (row >= 0 && row < SIZE && column >= 0 && column < SIZE) {
@@ -48,81 +61,7 @@ public class Board {
     }
 
     /**
-     * Gets the stone that is placed on a specific position.
-     *
-     * @param row    is the row of interest
-     * @param column is the column of interest
-     * @return the Stone that is placed on that position; can be EMPTY as well if no stone is placed
-     */
-    public Stone getStone(int row, int column) {
-        return board[row][column];
-    }
-
-    /**
-     * Checks whether the position is empty.
-     *
-     * @param row    is the row of interest
-     * @param column is the column of interest
-     * @return true if the position is empty; if not, return false
-     */
-    public boolean isEmptyPosition(int row, int column) {
-        if (getStone(row, column) != Stone.EMPTY) {
-            //TODO message misschien niet hier vermelden, want dan iedere keer bij removal ook melding.
-            System.out.println("This position is already in use: the stone on this position is " + getStone(row, column) + ".");
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Checks whether all positions on the board are filled with black and/or white stones.
-     *
-     * @return true if all positions are filled; if (at least) one position is still EMPTY, return false
-     */
-    public boolean isFull() {
-        for (int row = 0; row < SIZE; row++) {
-            for (int column = 0; column < SIZE; column++) {
-                if (board[row][column] == Stone.EMPTY) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Places the stone of interest on a specific position (after checking if this position is valid and if this
-     * position is empty).
-     *
-     * @param row    is the row of interest
-     * @param column is the column of interest
-     * @param stone  is the stone to place
-     */
-    public void placeStone(int row, int column, Stone stone) {
-        // First of all, the position should be valid (within the boundaries of the board). Furthermore, to be able to
-        // place a stone, this stone must be either BLACK or WHITE, and the position must be EMPTY.
-        if (isValidPosition(row, column) && stone != Stone.EMPTY && isEmptyPosition(row, column)) {
-            board[row][column] = stone;
-        }
-    }
-
-    /**
-     * Removes a stone from a specific position (after checking if this position is valid and a stone is available on
-     * this position).
-     *
-     * @param row    is the row of interest
-     * @param column is the column of interest
-     */
-    public void removeStone(int row, int column) {
-        // First of all, the position should be valid (within the boundaries of the board). Furthermore, to be able to
-        // remove a stone, the position must NOT be EMPTY.
-        if (isValidPosition(row, column) && !isEmptyPosition(row, column)) {
-            board[row][column] = Stone.EMPTY;
-        }
-    }
-
-    /**
-     * Creates a representation of the board of type String.
+     * Creates a representation of the board of type String (needed for checking ko rule).
      */
     public String toString() {
         String stringRepresentationOfBoard = "";
@@ -140,18 +79,85 @@ public class Board {
         return stringRepresentationOfBoard;
     }
 
+    /**
+     * Creates a copy of the current state of the board.
+     */
+    public Board copyBoard() {
+        Board copyBoard = new Board();
+        for (int row = 0; row < SIZE; row++) {
+            for (int column = 0; column < SIZE; column++) {
+                copyBoard.board[row][column] = board[row][column];
+            }
+        }
+        return copyBoard;
+    }
+
+    // Methods needed to actually play the game:
+
+    /**
+     * Places the stone of interest on a specific position (after checking if this position is valid and if this
+     * position is empty).
+     *
+     * @param row    is the row of interest;
+     * @param column is the column of interest;
+     * @param stone  is the stone to place.
+     */
+    public void placeStone(int row, int column, Stone stone) {
+        // First of all, the position should be valid (within the boundaries of the board). Furthermore, to be able to
+        // place a stone, this stone must be either BLACK or WHITE, and the position must be EMPTY.
+        if (isValidPosition(row, column) && stone != Stone.EMPTY && isEmptyPosition(row, column)) {
+            board[row][column] = stone;
+        }
+    }
+
+    /**
+     * Removes a stone from a specific position (after checking if this position is valid and a stone is available on
+     * this position).
+     *
+     * @param row    is the row of interest;
+     * @param column is the column of interest.
+     */
+    public void removeStone(int row, int column) {
+        // First of all, the position should be valid (within the boundaries of the board). Furthermore, to be able to
+        // remove a stone, the position must NOT be EMPTY.
+        if (isValidPosition(row, column) && !isEmptyPosition(row, column)) {
+            board[row][column] = Stone.EMPTY;
+        }
+    }
+
+    // Methods needed to check for game over:
+
+    /**
+     * Checks whether all positions on the board are filled with black and/or white stones.
+     *
+     * @return true if all positions are filled; if (at least) one position is still EMPTY, return false.
+     */
+    public boolean isFull() {
+        for (int row = 0; row < SIZE; row++) {
+            for (int column = 0; column < SIZE; column++) {
+                if (board[row][column] == Stone.EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Methods used to check whether stones are placed on the correct position:
+
+    /**
+     * Prints the state of the board, including all placed stones.
+     */
     public void printBoard() { // can be deleted in the end; only check to see how/if it works as expected.
         // print the column numbers on top
         System.out.print("   ");
         for (int column = 0; column < SIZE; column++) {
             System.out.printf("%-3s", column);
         }
-        System.out.println();
-
         // print the row numbers on the left side.
+        System.out.println();
         for (int row = 0; row < SIZE; row++) {
             System.out.printf("%-3s", row);
-
             // print the state of the field.
             for (int column = 0; column < SIZE; column++) {
                 if ((board[row][column]) == Stone.EMPTY) {
