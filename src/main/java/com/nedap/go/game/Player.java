a24f104f6706d967a28cf92777e7ff9504c8c99c
arjonne.laar <a.laar@student.utwente.nl>
2023-01-31 21:45:40 +0100

    Change in order of methods + improve documentation

--- src/main/java/com/nedap/go/game/Player.java
+++ src/main/java/com/nedap/go/game/Player.java
@@ -3,19 +3,21 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- * Represents the player of the GO game.
- * Each game has two players; either two human players, one human player and one computer player or two computer players.
- * Each player has a name and has a stone (either black or white).
+ * Represents the player of the GO game. Each game has two players; either two human players, one human player and one
+ * computer player or two computer players. Each This player has a name and has a stone (either black or white), and
+ * also a methods to determine a next move.
  */
 
 public class Player {
-    private String username;
-    private Stone stone;
+    private final String username;
+    private final Stone stone;
+    private int row;
+    private int column;
 
     /**
      * Constructor to create a new player. Each player has a name and uses either a black or a white stone.
      *
-     * @param username represents the name of the player
+     * @param username represents the name of the player;
      * @param stone    represents the stone this player uses.
      */
     public Player(String username, Stone stone) {
@@ -44,29 +46,28 @@ public class Player {
     /**
      * Determines the next move this player will make.
      *
-     * @param game is the game this player is playing
-     * @return the move this player can make. If no valid move is available, return null and pass
+     * @param game is the game this player is playing;
+     * @return the move this player can make. If no valid move is available, return null and pass.
      */
     public Position determineMove(Game game) {
         System.out.println("Do you want to make a move or pass? Type either MOVE or PASS:");
-        Scanner input = new Scanner(System.in);
-        int row = -1;
-        int column = -1;
-        if (input.nextLine().toUpperCase().equals("PASS")) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().toUpperCase();
+        if (input.equals("PASS")) {
             return null;
-        } else if (input.nextLine().toUpperCase().equals("MOVE")) {
+        } else if (input.equals("MOVE")) {
             System.out.println("On what intersection do you want to place your stone?");
             boolean correctInput = false;
             while (!correctInput) {
                 System.out.print("First, enter the row number (ranging between 1 and " + Board.SIZE + ": ");
                 try {
-                    row = (input.nextInt() - 1);
+                    row = (scanner.nextInt() - 1);
                 } catch (InputMismatchException e) {
                     System.out.println("Input must be a number between 1 and " + Board.SIZE + "!");
                 }
                 System.out.print("Now, enter the column number (ranging between 1 and " + (Board.SIZE) + ": ");
                 try {
-                    column = (input.nextInt() - 1);
+                    column = (scanner.nextInt() - 1);
                     correctInput = true;
                 } catch (InputMismatchException e) {
                     System.out.println("Input must be a number between 1 and " + Board.SIZE + "!");
