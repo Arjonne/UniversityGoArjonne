b857d4eb4dbd4c1a47f96775f8ea7bbafefcdd5d
arjonne.laar <a.laar@student.utwente.nl>
2023-01-31 21:44:02 +0100

    Change in order of methods + improve documentation

--- src/main/java/com/nedap/go/game/ComputerPlayer.java
+++ src/main/java/com/nedap/go/game/ComputerPlayer.java
@@ -1,8 +1,8 @@
 package com.nedap.go.game;
 
 /**
- * Represents the computer player of the GO game.
- * This player has a name and has a stone (either black or white), and also a methods to determine a next move.
+ * Represents the computer player of the GO game. This player has a name and has a stone (either black or white),
+ * and also a methods to determine a next move.
  */
 
 public class ComputerPlayer extends Player {
@@ -10,7 +10,7 @@ public class ComputerPlayer extends Player {
     /**
      * Constructor to create a new computer player. Each player has a name and uses either a black or a white stone.
      *
-     * @param username represents the name of the player
+     * @param username represents the name of the player;
      * @param stone    represents the stone this player uses.
      */
     public ComputerPlayer(String username, Stone stone) {
@@ -20,8 +20,8 @@ public class ComputerPlayer extends Player {
     /**
      * Determines the next move this player will make.
      *
-     * @param game is the game this player is playing
-     * @return the move this player can make. If no valid move is available, return null
+     * @param game is the game this player is playing;
+     * @return the move this player can make. If no valid move is available, return null.
      */
     @Override
     public Position determineMove(Game game) {
