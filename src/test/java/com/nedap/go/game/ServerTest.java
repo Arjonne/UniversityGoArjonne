2ae7ea7ebfa5d820b86f61bb702615e0203ed8b5
arjonne.laar <a.laar@student.utwente.nl>
2023-01-31 18:14:18 +0100

    Start with tests for server

---
+++ src/test/java/com/nedap/go/game/ServerTest.java
@@ -0,0 +1,35 @@
+package com.nedap.go.game;
+
+import com.nedap.go.server.Server;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ServerTest {
+
+    private Server server;
+
+    @BeforeEach
+    void setUp() {
+        server = new Server(0);
+    }
+
+    @Test
+    void testStartAndStopServer() {
+        // after creating the server, it is still closed:
+        assertFalse(server.isOpenForConnection());
+
+        // start the server; now, it should be open for connections:
+        server.start();
+        assertTrue(server.isOpenForConnection());
+        // also, the port number should be between 0 and 65535:
+        assertTrue(server.getPort() > 0);
+        assertTrue(server.getPort() <= 65535);
+
+        // after closing the server, it should not be open for connections anymore:
+        server.stop();
+        assertFalse(server.isOpenForConnection());
+    }
+}
+
